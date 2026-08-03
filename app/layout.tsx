673b6b8e0fe507026abbf1aa816d2d58e2602928
Ela Teknoloji <hytasarim@gmail.com>
2026-08-03 16:44:47 +0300

    tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr" className="scroll-smooth">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5KC74XTH');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${barlow.variable} ${barlowCondensed.variable} font-barlow antialiased bg-white text-text-dark relative`}>
         
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KC74XTH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* GOOGLE ANALYTICS (GA4) ENTEGRASYONU */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1PYDRGRGS4"
